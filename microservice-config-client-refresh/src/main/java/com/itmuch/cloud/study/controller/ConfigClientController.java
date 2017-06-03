f6138616f33605137ed2d655097c38d1c93c5daa
Spring Cao <Spring Cao@93YG1G2.yixin.dk>
2017-06-03 14:19:24 +0800

    增加注释并说明如何实现刷新

--- microservice-config-client-refresh/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
+++ microservice-config-client-refresh/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
@@ -5,6 +5,15 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * 可支持刷新的config server客户端使用示例
+ * @description 
+ *    若要获取config server端更新后的值，需要post方式发送到 http://ip:port/refresh
+ *    例如：http://localhost:8081/refresh
+ * @author Spring Cao
+ * @date 2017年6月3日 下午2:17:06
+ */
 @RestController
 @RefreshScope
 public class ConfigClientController {
