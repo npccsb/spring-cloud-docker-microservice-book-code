2a70cee7f1378547408fbf706800d53f3337440d
Spring Cao <Spring Cao@93YG1G2.yixin.dk>
2017-06-05 08:55:16 +0800

    增加注释内容

--- microservice-config-client-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigClientApplication.java
+++ microservice-config-client-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigClientApplication.java
@@ -3,6 +3,16 @@ package com.itmuch.cloud.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 基于SpringCloudBus的ConfigServer刷新机制的客户端
+ * @description 
+ * 
+ * 根据“根”上下文优先加载原则，resources下的bootstrap.yml会比application.yml中的内容提前加载
+ * 所以，需要将config server地址放入在bootstrap.yml中实现提前加载
+ * 
+ * @author Spring Cao
+ * @date 2017年6月5日 上午8:47:34
+ */
 @SpringBootApplication
 public class ConfigClientApplication {
   public static void main(String[] args) {
--- microservice-config-server-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
+++ microservice-config-server-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 基于SpringCloudBus的ConfigServer刷新机制的服务器端
+ * @description
+ * 根据“根”上下文优先加载原则，resources下的bootstrap.yml会比application.yml中的内容提前加载
+ * 所以，需要将config server地址放入在bootstrap.yml中实现提前加载
+ *  
+ * @author Spring Cao
+ * @date 2017年6月5日 上午8:50:31
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
