ed94a17afb29e80c708743c7dabb8adbeea2444d
Spring Cao <Spring Cao@93YG1G2.yixin.dk>
2017-06-05 14:27:17 +0800

    优化注释说明如何刷新cloud bus

--- microservice-config-server-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
+++ microservice-config-server-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
@@ -11,6 +11,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * 所以，需要将config server地址放入在bootstrap.yml中实现提前加载
  * 2.本例采用rabbitmq(也可以是kafka，但需要修改pom依赖从amqp到kafka)来实现cloud bus，
  * rabbitmq直接pull自docker.io提供的官方rabbitmq镜像。
+ * 3.使用postman发送post信息到http://localhost:8081/bus/refresh 来实现通知bus上的全部节点进行配置刷新
+ * 
+ * ps：若不需要cloud bus时只能通过指向具体的应用地址才能刷新这个应用的配置，http://localhost:8080/refresh
  *  
  * @author Spring Cao
  * @date 2017年6月5日 上午8:50:31
